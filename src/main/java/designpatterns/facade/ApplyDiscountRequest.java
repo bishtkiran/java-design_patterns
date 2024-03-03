50a003191d85cf0c3223def27dd24ca45fa9dbaa
Kiran Bisht <kiranbisht338@gmail.com>
2024-03-03 16:18:51 +0530

    facade design pattern

---
+++ src/main/java/designpatterns/facade/ApplyDiscountRequest.java
@@ -0,0 +1,38 @@
+package designpatterns.facade;
+
+public class ApplyDiscountRequest {
+
+    private long idCustomer;
+    private long idProduct;
+    private long idDiscount;
+
+    public ApplyDiscountRequest(long idCustomer, long idProduct, long idDiscount) {
+        this.idCustomer = idCustomer;
+        this.idProduct = idProduct;
+        this.idDiscount = idDiscount;
+    }
+
+    public long getIdCustomer() {
+        return idCustomer;
+    }
+
+    public void setIdCustomer(long idCustomer) {
+        this.idCustomer = idCustomer;
+    }
+
+    public long getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(long idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public long getIdDiscount() {
+        return idDiscount;
+    }
+
+    public void setIdDiscount(long idDiscount) {
+        this.idDiscount = idDiscount;
+    }
+}
---
+++ src/main/java/designpatterns/facade/Product.java
@@ -0,0 +1,40 @@
+package designpatterns.facade;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private long idProduct;
+    private String name;
+    private BigDecimal price;
+
+    public Product(long idProduct, String name, BigDecimal price) {
+        this.idProduct = idProduct;
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(long idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
