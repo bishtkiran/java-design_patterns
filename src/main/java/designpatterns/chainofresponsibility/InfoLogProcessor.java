20b85197763b74544af064eaf19c04b2e20d73ba
Kiran Bisht <kiranbisht338@gmail.com>
2024-03-09 15:10:34 +0530

    chain of responsibility design pattern

---
+++ src/main/java/designpatterns/chainofresponsibility/InfoLogProcessor.java
@@ -0,0 +1,16 @@
+package designpatterns.chainofresponsibility;
+
+public class InfoLogProcessor extends LogProcessor{
+
+    InfoLogProcessor(LogProcessor nextLogProcessor) {
+        super(nextLogProcessor);
+    }
+
+    @Override
+    public void log(int logLevel, String message) {
+        if(logLevel == INFO)
+            System.out.println("INFO: " + message);
+        else
+            super.log(logLevel, message);
+    }
+}
